52da91abdd616e22556e4a0d9250b58f214026e8
Gustav Lau <gustavlau@gmail.com>
2021-03-13 13:34:54 -0500

    add nav bar js

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.querySelector(".nav").classList.add("affix");
+    document.getElementById("nav-link-main").style.fontSize="2em";
+  } else {
+    document.querySelector(".nav").classList.remove("affix");
+    document.getElementById("nav-link-main").style.fontSize="1em";
+  }
+}
