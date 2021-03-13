7ec153d729c78f2738c05b2cbdfb354e0e8f8501
Gustav Lau <gustavlau@gmail.com>
2021-03-13 18:18:44 -0500

    even listeners and logic for nav

--- script.js
+++ script.js
@@ -9,11 +9,25 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.querySelector(".nav").classList.add("affix");
     document.getElementById("nav-link-main").style.fontSize="2em";
   } else {
     document.querySelector(".nav").classList.remove("affix");
     document.getElementById("nav-link-main").style.fontSize="1em";
   }
+}
+
+let hamburgerMenu = document.getElementById("hamburger");
+hamburgerMenu.addEventListener('click',mobileShow);
+
+function mobileShow (){
+  let mobileLinks = Array.from(document.querySelector(".nav-links-mobile").children);
+  mobileLinks.forEach((mobileLinksChildren)=>{
+    if(mobileLinksChildren.classList.contains("nav-hide")){
+      mobileLinksChildren.classList.remove("nav-hide");
+    } else {
+      mobileLinksChildren.classList.add("nav-hide");
+    }
+  });  
 }
