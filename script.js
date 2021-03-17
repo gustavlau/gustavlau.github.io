5a46d1aeb512d554ea466353ea5a5e995c362b78
Gustav Lau <gustavlau@gmail.com>
2021-03-17 16:20:32 -0400

    add dual hover effect for gifs

--- script.js
+++ script.js
@@ -35,9 +35,44 @@ function mobileShow (){
   });  
 }
 
-new Freezeframe({
-  trigger: 'hover',
-  overlay: false,
-  responsive: true,
-  warnings: false
-});
+
+
+// new Freezeframe({
+//   trigger: 'hover',
+//   overlay: false,
+//   responsive: true,
+//   warnings: false
+// });
+
+// let buttonHover =[];
+// for(i=1;i<5;i++){
+//   buttonHover[i]= new Freezeframe(`.ff${i}`,{
+//     trigger:'hover'});
+// }
+
+
+let buttonHover1 = new Freezeframe('.ff1',{
+  trigger:'hover'});
+
+let buttonHover2 = new Freezeframe('.ff2',{
+  trigger:'hover'});
+
+let buttonHover3 = new Freezeframe('.ff3',{
+  trigger:'hover'});
+
+let buttonHover4 = new Freezeframe('.ff4',{
+  trigger:'hover'});
+      
+
+
+document.querySelector("#ff1-btn").addEventListener('mouseover',()=>{buttonHover1.start()});
+document.querySelector("#ff1-btn").addEventListener('mouseout',()=>{buttonHover1.stop()});
+
+document.querySelector("#ff2-btn").addEventListener('mouseover',()=>{buttonHover2.start()});
+document.querySelector("#ff2-btn").addEventListener('mouseout',()=>{buttonHover2.stop()});
+
+document.querySelector("#ff3-btn").addEventListener('mouseover',()=>{buttonHover3.start()});
+document.querySelector("#ff3-btn").addEventListener('mouseout',()=>{buttonHover3.stop()});
+
+document.querySelector("#ff4-btn").addEventListener('mouseover',()=>{buttonHover4.start()});
+document.querySelector("#ff4-btn").addEventListener('mouseout',()=>{buttonHover4.stop()});
